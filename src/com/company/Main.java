de41f90ceff0f0c23e48741ae0620990194c34b4
PauliPiedra <pauliipiedra@gmail.com>
2021-08-21 12:09:28 -0300

    primer commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nombre = "Paula";
+        int edad = 31;
+        float salario = 70.00f;
+        boolean carnet = true;
+
+        System.out.println("Nombre = " + nombre + "  Edad = " + edad + "  Salario = $" + salario + "  Carnet = " + carnet );
+
+
+
+
+
+    }
+}
